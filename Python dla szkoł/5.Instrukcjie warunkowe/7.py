fd6c2084acf2429f84ffe2418bc2fa61bad83e1f
SzymonRog <rogalaszymon78@gmail.com>
2026-02-18 17:28:45 +0000

    123

--- Python dla szkoł/5.Instrukcjie warunkowe/7.py
+++ Python dla szkoł/5.Instrukcjie warunkowe/7.py
@@ -1,11 +1,23 @@
-def setPower( pm25, pm10 ):
-    m = 0
-    if pm25 <= 12 or pm10 <= 20:
-        m = 0
-    elif pm25 >= 13 and pm25 <= 24 or pm10 >= 21 and pm10 <= 50:
-        m = 1
-    elif pm25 >= 25 and pm25 <= 36 or pm10 >= 51 and pm10 <= 80 :
-        m = 2
-    elif pm25 >= 36 or pm10 >= 80:
-        m = 3
-    return m
+def setPower(pm25, pm10):
+    # określenie indeksu dla PM2.5
+    if pm25 <= 12:
+        index25 = 0
+    elif pm25 <= 24:
+        index25 = 1
+    elif pm25 <= 36:
+        index25 = 2
+    else:
+        index25 = 3
+
+    # określenie indeksu dla PM10
+    if pm10 <= 20:
+        index10 = 0
+    elif pm10 <= 50:
+        index10 = 1
+    elif pm10 <= 80:
+        index10 = 2
+    else:
+        index10 = 3
+
+    # zwracamy gorszy (większy) indeks
+    return max(index25, index10)
